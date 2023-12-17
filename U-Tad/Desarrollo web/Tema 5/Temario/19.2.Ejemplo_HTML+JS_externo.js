6eb7dcac94d1a4b4159649a938ce441f08854843
Ismael <153450550+ismaelucky342@users.noreply.github.com>
2023-12-17 13:53:46 +0100

    17/12/23
    actualizacion tema 5 web

---
+++ U-Tad/Desarrollo web/Tema 5/Temario/19.2.Ejemplo_HTML+JS_externo.js
@@ -0,0 +1,17 @@
+// Definición de la función sumar
+function sumar(a, b) {
+    var suma = a + b;
+    return suma;
+}
+
+// Llamada a la función sumar con los argumentos 2 y 3
+var resultado1 = sumar(2, 3);
+console.log("Resultado 1: " + resultado1);
+
+// Llamada a la función sumar con los argumentos 5 y 7
+var resultado2 = sumar(5, 7);
+console.log("Resultado 2: " + resultado2);
+
+// Llamada a la función sumar con los argumentos 1 y 2
+var resultado3 = sumar(1, 2);
+console.log("Resultado 3: " + resultado3);
