c1269fb9f2e4710aada2888c97c8c62105245090
Ismael <153450550+ismaelucky342@users.noreply.github.com>
2024-01-23 14:16:12 +0100

    14 16

--- U-Tad/Programación-1/Preparacion-examen/Arrays.c
+++ U-Tad/Programación-1/Preparacion-examen/Arrays.c
@@ -2,6 +2,14 @@
 
 int     main()
 {
-    
+	int	sizeA; 
+	printf("tamaño arreglo\n",arreglo[2]); 
+	scanf("i%", &sizeA); 
+	int age[sizeA];
+	for(int	i = 0; i < sizeA, i++); 
+	{
+		printf
+	}
+	return 0; 
     
 }
