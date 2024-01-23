38f155567087d5445bab31d201a4c1689e5e3bb6
Ismael <153450550+ismaelucky342@users.noreply.github.com>
2024-01-23 22:59:00 +0100

    22 58

---
+++ U-Tad/Programación-1/Preparacion-examen/punteros.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int		main()
+{
+	int	a = 2;
+	int *apt = &a;
+	
+	
+	printf("%i\n", *apt);
+	
+	
+	
+	return 0; 
+}
---
+++ U-Tad/Programación-1/Preparacion-examen/punteros2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int	main()
+{
+	int	a = 2; 
+	int	*apt = &a; 
+
+	printf("%p\n", apt); 
+	num = cubo(num);
+
+	return 0;
+
+}
+
+int	cubo(int	num)
+{
+
+}
