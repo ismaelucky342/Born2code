33a82d4a3e6b7e90043821c927a4429fa6d43aa6
Ismael <153450550+ismaelucky342@users.noreply.github.com>
2024-01-31 11:44:26 +0100

    11 44

--- U-Tad/Programación-1/Preparacion-examen/Intensivo/contadorVocales.c
+++ U-Tad/Programación-1/Preparacion-examen/Intensivo/contadorVocales.c
@@ -14,14 +14,20 @@ int     main()
     return 0; 
 }
 
-int     recogerCadena(char cadena[])
-{
-    int i = 0; 
-    char c;  
-
-    printf("Introduce una frase: ");
-    while((c = getchar() ) != '\n' && i < 30)
-    {
+int recogeCadena(char cadena[]) {
+    printf("Introduce una cadena de texto: ");
+    int i = 0;
+    char c;
+    while ((c = getchar()) != '\n' && i < 30) {
         cadena[i++] = c;
     }
+    cadena[i] = '\0';
+
+    if (c != '\n') {
+        printf("La cadena es demasiado larga. Inténtalo de nuevo.\n");
+        while (getchar() != '\n');
+        return 0;
+    }
+
+    return i;
 }
