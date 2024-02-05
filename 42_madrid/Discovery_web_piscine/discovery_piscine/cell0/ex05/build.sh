e76225b3c6913a3cf08e0c480e3769745f405351
Ismael <153450550+ismaelucky342@users.noreply.github.com>
2024-02-05 12:58:34 +0100

    5/2/2024     12:58
    actualización piscina discovery, mal subida y traspaso a notion de código

---
+++ 42_madrid/Discovery_web_piscine/discovery_piscine/cell0/ex05/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ $# -eq 0 ]
+then
+	echo "No arguments supplied"
+else
+	for i in $@
+	do
+		mkdir "ex$i"
+	done
+fi
---
+++ U-Tad/Programación-1/EXAMEN/Mis_Practicas/reutilizable.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int 	main()
+{
+	char c = "hola";  
+	
+	printf("edad es igual a: %c", c);
+	return 0; 
+}
