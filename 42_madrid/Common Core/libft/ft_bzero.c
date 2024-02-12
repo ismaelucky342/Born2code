e9017989f4903f40179eacd84846312e07e35286
Ismael <153450550+ismaelucky342@users.noreply.github.com>
2024-02-12 12:29:21 +0100

    12 29

--- 42_madrid/Common Core/libft/ft_bzero.c
+++ 42_madrid/Common Core/libft/ft_bzero.c
@@ -16,6 +16,4 @@ void	ft_bzero(void *s, size_t n)
 {
 	ft_memset(s, 0, n);
 }
-/* Definición de la función bzero donde se define al puntero *s
- * que apunta al inicio del bloque de memoria que se va a anular,
- * y size_t n se determinará la cantidad de bytes que se anularan*/
+
